1f19e585ba7f365ade833f74515e85c6635b84c2
vinodmethre78-oss <vinodmethre78@gmail.com>
2026-04-01 11:34:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_albums():
     response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
         "data": response.json(),
-        "status": "success",
+        "status": "successfull message",
         "status_code": 200
     })
 
